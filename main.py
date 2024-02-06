994b770656d91d8b852bb40c65cc2636f4a57c9f
Dheeraj9595 <Dheeraj.959595@gmail.com>
2024-02-06 14:43:07 +0530

    added upload function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, render_template, request, jsonify
 from db import SessionLocal, User, Order
 from pydantic import BaseModel
-import json
 import bcrypt
+import os
 
 app = Flask(__name__)
 
@@ -136,7 +136,38 @@ def delete_order(order_id: int):
     return {"message": "Order deleted successfully....!!!"}
 
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
+    return render_template('upload_form.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    breakpoint()
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "No selected file"})
+
+    if file and allowed_file(file.filename):
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return jsonify({"message": "File uploaded successfully"})
+
+    return jsonify({"error": "File type not allowed"})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
