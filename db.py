effcf5c56e2c4f417f3ee0bd360403bd8eb6e768
Dheeraj9595 <Dheeraj.959595@gmail.com>
2024-02-07 19:37:07 +0530

    pep8 changes

--- db.py
+++ db.py
@@ -17,7 +17,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), index=True)
     password = Column(String(200))
-
     # Establish a one-to-many relationship with orders
     orders = relationship("Order", back_populates="user")
 
@@ -27,13 +26,10 @@ class Order(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), index=True)
     quantity = Column(Integer, index=True)
-
     # Establish a foreign key relationship with users
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship("User", back_populates="orders")
 
 
 # Create database tables
-
-
 Base.metadata.create_all(bind=engine)
