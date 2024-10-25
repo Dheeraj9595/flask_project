1d112fa0a30ac9a59506a0d0fbe2bceb7860d3d9
Dheeraj9595 <dheeraj.959595@gmail.com>
2024-10-25 21:49:30 +0530

    added utils function

--- utils.py
+++ utils.py
@@ -3,3 +3,25 @@ def greet():
 
 a = greet()
 print(a)
+
+import time 
+  
+  
+def timeis(func): 
+    '''Decorator that reports the execution time.'''
+  
+    def wrap(*args, **kwargs): 
+        start = time.time() 
+        result = func(*args, **kwargs) 
+        end = time.time() 
+          
+        print(func.__name__, end-start) 
+        return result 
+    return wrap 
+
+@timeis
+def greet():
+    print('hello')
+
+
+obj = greet()    
